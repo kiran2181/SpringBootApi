222ba6faeb0d341f08092160a3913a644655c02d
kiran2181 <kpawar0632@gmail.com>
2025-04-10 13:43:36 +0530

    initial message

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpringBootApi
 Spring Boot Operation
+
+Rest Api
---
+++ SpringBootApiex/src/main/java/com/fullstack/SpringBootApiexApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiexApplication.class, args);
+	}
+
+}
